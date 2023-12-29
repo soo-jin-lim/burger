13c6b04a5301def87b9dd4c717768a74133316bd
soo-jin-lim <123914453+soo-jin-lim@users.noreply.github.com>
2023-12-29 15:13:11 +0900

    .

---
+++ hamberger/src/main/java/com/example/hamberger/HambergerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hamberger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HambergerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HambergerApplication.class, args);
+    }
+
+}
